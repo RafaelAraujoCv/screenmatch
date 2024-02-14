3b23abe3438c96b0ff50f61db3bb7e4372e5842a
Rafael Araujo <rafaelaraujocv@hotmail.com>
2024-02-14 09:24:52 -0300

    First steps in java

---
+++ src/main/java/br/com/work/Screenmatch.java
@@ -0,0 +1,13 @@
+package br.com.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/work/ScreenmatchTests.java
@@ -0,0 +1,13 @@
+package br.com.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
